1df0d8bf6529ea69648ec6b3ce03b79e86eee069
Tristan vera <tristan.vera19@gmail.com>
2024-09-10 16:01:04 +0200

    change authOptions

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,46 +1,22 @@
+import { createUserWithAccount } from "@/utils/user";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
 export const POST = async (req) => {
   try {
     const { firstName, lastName, password, email } = await req.json();
-    console.log("Received data:", { firstName, lastName, password, email });
+    console.log("Received data:", { firstName, lastName, password, email }); // Log pour vérifier les données reçues
 
     const existingUser = await getUserByEmail(email);
-
     if (existingUser) {
-      // Vérifier si un compte Google existe déjà pour cet utilisateur
-      const googleAccount = existingUser.accounts.find(
-        (account) => account.provider === "google"
-      );
-
-      if (googleAccount) {
-        // Si un compte Google existe, ajouter un mot de passe à cet utilisateur
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        await prisma.user.update({
-          where: { email },
-          data: {
-            password: hashedPassword, // Ajouter le mot de passe
-            firstName: firstName || existingUser.firstName, // Mettre à jour les autres infos si elles manquent
-            lastName: lastName || existingUser.lastName,
-          },
-        });
-
-        return NextResponse.json(
-          {
-            message: "Mot de passe ajouté au compte existant.",
-          },
-          { status: 200 }
-        );
-      }
-
       return NextResponse.json(
         {
-          message: "Email is already in use.",
+          message: "Email is already in use",
         },
         { status: 400 }
       );
     }
 
-    // Si aucun utilisateur n'existe, créer un nouvel utilisateur avec des credentials
     const newUser = await createUserWithAccount({
       firstName,
       lastName,
@@ -68,10 +44,8 @@ export const POST = async (req) => {
 
 export async function getUserByEmail(email) {
   try {
-    // Trouver l'utilisateur avec l'email, y compris les comptes associés
     const user = await prisma.user.findUnique({
       where: { email },
-      include: { accounts: true }, // Inclure les comptes associés
     });
     return user;
   } catch (error) {
